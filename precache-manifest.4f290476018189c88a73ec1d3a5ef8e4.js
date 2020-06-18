0c50f33c6ebb61f09e5118227b21b72380776d41
Alejandro Villa <alexvigro92@gmail.com>
2020-06-17 20:10:28 -0500

    release (v1.0.0.0): Added Redux, SCSS and Spanish

---
+++ precache-manifest.4f290476018189c88a73ec1d3a5ef8e4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fef6f54832780fbca528bc7359cafbb",
+    "url": "/alejandrovilla/index.html"
+  },
+  {
+    "revision": "a26fac919746567e5745",
+    "url": "/alejandrovilla/static/css/main.5dd581f1.chunk.css"
+  },
+  {
+    "revision": "58063def49b26f2fa446",
+    "url": "/alejandrovilla/static/js/2.b0cdd536.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alejandrovilla/static/js/2.b0cdd536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26fac919746567e5745",
+    "url": "/alejandrovilla/static/js/main.32a70bc3.chunk.js"
+  },
+  {
+    "revision": "da5d9e86b4b2ef1345d9",
+    "url": "/alejandrovilla/static/js/runtime-main.b5754d30.js"
+  },
+  {
+    "revision": "ad57b263d800f5cb7b737d27af8cc0c9",
+    "url": "/alejandrovilla/static/media/Alejandro Villa Resume.ad57b263.pdf"
+  },
+  {
+    "revision": "587a3d166355ebc13806cc2217ffc33c",
+    "url": "/alejandrovilla/static/media/VillaSimpson.587a3d16.png"
+  },
+  {
+    "revision": "ac5e6fae7e154df1a052d0bc55ff9577",
+    "url": "/alejandrovilla/static/media/angular.ac5e6fae.png"
+  },
+  {
+    "revision": "048546ac34d817fa15fd29e8c26146c8",
+    "url": "/alejandrovilla/static/media/background.048546ac.jpg"
+  },
+  {
+    "revision": "946fc4d292cde1f98be7774f650b6a16",
+    "url": "/alejandrovilla/static/media/database.946fc4d2.png"
+  },
+  {
+    "revision": "2ae36a777095a5adc5b0a03331a9b961",
+    "url": "/alejandrovilla/static/media/devops.2ae36a77.png"
+  },
+  {
+    "revision": "2ca65f837d3b4bd8306ddf3e713f3b7f",
+    "url": "/alejandrovilla/static/media/docker.2ca65f83.png"
+  },
+  {
+    "revision": "8c563a48f876a8b38aafd02af499c2e4",
+    "url": "/alejandrovilla/static/media/ibmCloud.8c563a48.png"
+  },
+  {
+    "revision": "ece283ab0fdcd76ddbb684fe1d147a9a",
+    "url": "/alejandrovilla/static/media/iot.ece283ab.png"
+  },
+  {
+    "revision": "4655ae6f8193d16e391f8971119decee",
+    "url": "/alejandrovilla/static/media/kubernetes.4655ae6f.png"
+  },
+  {
+    "revision": "016733cc8e5630480c48a5cc58aa23da",
+    "url": "/alejandrovilla/static/media/mavg1.016733cc.jpg"
+  },
+  {
+    "revision": "fa84f6eb31f2290cbccbeb9cfdf1d9ab",
+    "url": "/alejandrovilla/static/media/mavg2.fa84f6eb.jpg"
+  },
+  {
+    "revision": "1c82d68e59a5eba1f634b3e09f4b3b98",
+    "url": "/alejandrovilla/static/media/mavg3.1c82d68e.jpg"
+  },
+  {
+    "revision": "a85490e00db16770252cefab52493a55",
+    "url": "/alejandrovilla/static/media/mavg4.a85490e0.jpg"
+  },
+  {
+    "revision": "98c045507264a983aaacedb2ac11a9db",
+    "url": "/alejandrovilla/static/media/mavg5.98c04550.jpg"
+  },
+  {
+    "revision": "6cbe72689b1f8eed6f75d5cb80ef83b3",
+    "url": "/alejandrovilla/static/media/nodejs.6cbe7268.png"
+  },
+  {
+    "revision": "f99b25da7406c376f88bcf09bfa02b06",
+    "url": "/alejandrovilla/static/media/openshift.f99b25da.png"
+  },
+  {
+    "revision": "5b14848a3654b0099a325fad076e9657",
+    "url": "/alejandrovilla/static/media/rabbit.5b14848a.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/alejandrovilla/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/alejandrovilla/static/media/redux.8251fe76.png"
+  }
+]);
